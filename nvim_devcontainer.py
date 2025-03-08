3f8eda3156a9517f023062a21e337e91b43b267d
Ben Davis <bendavis78@gmail.com>
2025-03-08 15:14:07 -0600

    add ability to use custom dockerfile for build

--- nvim_devcontainer.py
+++ nvim_devcontainer.py
@@ -1,16 +1,43 @@
 #!/usr/bin/env python3
 import argparse
+import logging
 import os
 import subprocess
 import sys
 import tempfile
+from contextlib import contextmanager
 from io import StringIO
 from pathlib import Path
 from tempfile import TemporaryDirectory
-from typing import Any
+from typing import IO, Any, Iterator
 
 from ruamel.yaml import YAML
 from xdg import BaseDirectory as xdg  # type: ignore
+
+# Set up logging to write to stderr
+log = logging.getLogger(__name__)
+
+class ColorFormatter(logging.Formatter):
+    RED = "\033[91m"
+    GREEN = "\033[92m"
+    BLUE = "\033[94m"
+    RESET = "\033[0m"
+    ORANGE = "\033[33m"
+
+    def format(self, record):
+        msg = super().format(record)
+        if record.levelno == logging.ERROR:
+            return f"{self.RED}⊗{self.RESET} {msg}"
+        elif record.levelno == logging.WARNING:
+            return f"{self.ORANGE}⚠{self.RESET} {msg}"
+        elif record.levelno == logging.DEBUG:
+            return f"{self.BLUE}➤{self.RESET} {msg}"
+        return f"{self.GREEN}ⓘ{self.RESET} {msg}"
+
+handler = logging.StreamHandler(sys.stderr)
+handler.setFormatter(ColorFormatter())
+log.setLevel(os.environ.get("LOG_LEVEL", logging.INFO))
+log.addHandler(handler)
 
 script_dir = os.path.dirname(os.path.realpath(__file__))
 
@@ -47,31 +74,67 @@ class CommandError(Exception):
         super().__init__(message)
 
 
+class Dockerfile:
+    def __init__(self, base_image_name: str, filename: str | None = None):
+        self.base_image_name = base_image_name
+        self.filename = filename
+        self.file = None
+
+    def __enter__(self) -> IO[Any]:
+        self.file = self._write_dockerfile()
+        return self.file
+
+    def __exit__(self, *_) -> None:
+        if self.file and self.file is not sys.stdout:
+            self.file.close()
+
+    def _write_dockerfile(self) -> IO[Any]:
+        sys.stdout.flush()
+
+        with open(os.path.join(script_dir, "Dockerfile.amd64"), "r") as f:
+            template_contents = f.read()
+
+        new_dockerfile_contents = template_contents.replace(
+            "%%__BASE_IMAGE__%%", self.base_image_name
+        )
+
+        if self.filename == "-":
+            sys.stdout.write(new_dockerfile_contents)
+            sys.stdout.flush()
+            return sys.stdout
+        elif self.filename:
+            file = open(self.filename, "w")
+            file.write(new_dockerfile_contents)
+            file.flush()
+            return file
+        else:
+            file = tempfile.NamedTemporaryFile(delete=False)
+            file.write(new_dockerfile_contents.encode())
+            file.flush()
+            return file
+
+    def write(self) -> IO[Any]:
+        return self._write_dockerfile()
+
+
 def build(
     base_image_name: str,
     tag: str,
     context_dir: Path | str,
-    no_cache: bool = False,
+    args: list | None = None,
+    out_filename: str | None = None,
 ) -> None:
-    with open(os.path.join(script_dir, "Dockerfile.amd64"), "r") as f:
-        template_contents = f.read()
+    args = args or []
 
-    new_dockerfile_contents = template_contents.replace(
-        "%%__BASE_IMAGE__%%", base_image_name
-    )
-
-    with tempfile.NamedTemporaryFile() as f:
-        f.write(new_dockerfile_contents.encode())
-        f.flush()
+    with Dockerfile(base_image_name, out_filename) as f:
         tag = tag or f"{base_image_name}:nvim-devcontainer"
         cmd: list[str] = ["docker", "build"]
-        if no_cache:
-            cmd.append("--no-cache")
         cmd.extend(["-t", tag, "-f", f.name, str(context_dir)])
-        print(" ".join(cmd))
+        cmd.extend(args)
+        log.debug(" ".join(cmd))
         subprocess.run(cmd, check=True)
 
-    print(f'Image "{tag}" is now available')
+    log.info('Built image "%s"', tag)
 
 
 def compose(args: argparse.Namespace) -> None:
@@ -85,20 +148,26 @@ def compose(args: argparse.Namespace) -> None:
     # load source service from current docker-comose config
     if os.path.exists(compose_override_file):
         with open(compose_override_file) as f:
-            compose_override_config = yaml.load(f)
+            compose_override_config = yaml.load(f) or {}
     else:
         compose_override_config = yaml.load(StringIO("services:"))
 
     with open(compose_file) as f:
-        comopse_config = yaml.load(f)
+        compose_config = yaml.load(f) or {}
 
     # Create new service in compose_override_config using deep clone of source service
-    source_service = comopse_config["services"][args.source_service]
+    source_service = compose_config["services"][args.source_service]
     new_service = source_service.copy()
 
     # Get the context dir
-    default_context_dir = os.path.dirname(args.compose_file)
-    context_dir = source_service.get("build", {}).get("context", default_context_dir)
+    build_config = source_service.get("build", {})
+    context_dir = build_config.get("context")
+
+    # Require context_dir
+    if not context_dir:
+        raise CommandError(
+            f"Could not determine context dir from service `{args.source_service}`"
+        )
 
     # Update env vars in new service
     env = new_service.get("environment", [])
@@ -130,8 +199,6 @@ def compose(args: argparse.Namespace) -> None:
     source_image_base = source_image.split(":")[0]
     new_service["image"] = f"{source_image_base}:nvim-devcontainer"
 
-    build(source_image, new_service["image"], context_dir, no_cache=args.no_cache)
-
     new_service["stdin_open"] = True
     new_service["tty"] = True
     new_service["entrypoint"] = "nvim"
@@ -162,11 +229,40 @@ def compose(args: argparse.Namespace) -> None:
     compose_override_config["services"] = compose_override_config.get("services") or {}
     compose_override_config["services"][args.name] = new_service
 
+    if args.no_build and args.out_filename:
+        log.debug(f"Setting build settings to use {args.out_filename}")
+        new_service["build"] = source_service.get("build", {})
+        # get context_dir relative to compose file
+        new_service["build"]["context"] = context_dir
+        new_service["build"]["dockerfile"] = args.out_filename
+        new_service["build"]["target"] = "nvim-devcontainer"
+
+
+    else:
+        build_args = args.build_args and args.build_args[1:] or []
+        build(
+            source_image,
+            new_service["image"],
+            context_dir,
+            args=build_args,
+            out_filename=args.out_filename,
+        )
+
+
+    if args.out_filename:
+        Dockerfile(source_image, args.out_filename).write()
+        if args.out_filename != "-":
+            log.info("Dockerfile written to %s", args.out_filename)
+        else:
+            log.info("Dockerfile written to stdout")
+
     with open(compose_override_file, "w") as f:
         yaml.dump(compose_override_config, f)
 
-    print(
-        f"Configured service '{args.name}' in {os.path.relpath(compose_override_file)}"
+    log.info(
+        f"Configured service '%s' in %s",
+        args.name,
+        os.path.relpath(compose_override_file),
     )
 
 
@@ -182,10 +278,17 @@ def main() -> None:
         help="Build context directory",
         nargs="?",
     )
+    build_parser.add_argument("build_args", nargs=argparse.REMAINDER, help="Args to pass to docker build")
     build_parser.add_argument(
-        "--no-cache",
-        action="store_true",
-        help="Do not use cache when building the image",
+        "-o",
+        dest="out_filename",
+        type=str, 
+        help="Output path for generated Dockerfile (uses temp file by default)",
+    )
+    build_parser.add_argument(
+        "--context-dir",
+        type=str,
+        help="Context directory for the build (default: use context from source service)",
     )
 
     compose_parser = subparsers.add_parser(
@@ -219,6 +322,18 @@ def main() -> None:
         action="store_true",
         help="Do not use cache when building the image",
     )
+    compose_parser.add_argument(
+        "-o",
+        dest="out_filename",
+        type=str, 
+        help="Output path for generated Dockerfile (uses temp file by default)",
+    )
+    compose_parser.add_argument(
+        "--no-build",
+        action="store_true",
+        help="Output path for generated Dockerfile (uses temp file by default)",
+    )
+    compose_parser.add_argument("build_args", nargs=argparse.REMAINDER, help="Args to pass to docker build")
 
     args = parser.parse_args()
 
@@ -226,10 +341,11 @@ def main() -> None:
         if args.command == "build":
             tag_base = args.base_image.split(":")[0]
             tag = f"{tag_base}:nvim-devcontainer"
+            build_args = args.build_args and args.build_args[1:] or []
 
             if args.directory is None:
                 with TemporaryDirectory() as tmpdir:
-                    build(args.base_image, tag, tmpdir, no_cache=args.no_cache)
+                    build(args.base_image, tag, tmpdir, args=build_args)
             else:
                 build(args.base_image, tag, args.directory)
 
